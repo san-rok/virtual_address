466531ec5202c99a13a1b00e564fe438a9a461fd
san-rok <135208296+san-rok@users.noreply.github.com>
2023-08-01 18:39:33 +0200

    test: va: 0x970b

--- src/main.rs
+++ src/main.rs
@@ -651,12 +651,12 @@ fn main() {
     let path = String::from("/home/san-rok/projects/testtest/target/debug/testtest");
     let binary = Binary::from_elf(path);
 
-    let virtual_address: u64 =  0x8840;
+    let virtual_address: u64 =  0x970b;
     // test: 0x88cb, 0x8870, 0x88b0, 0x8a0d, 0x893e, 0x88f0, 0x8c81, 0x8840
 
     let cfg: ControlFlowGraph = ControlFlowGraph::from_address(&binary, virtual_address);
 
-    let dominators = simple_fast(&cfg, virtual_address);
+    // let dominators = simple_fast(&cfg, virtual_address);
 
     let mut f = std::fs::File::create("/home/san-rok/projects/virtual_address/virtual_address.dot").unwrap();
     cfg.render_to(&mut f).unwrap();
